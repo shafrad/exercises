e8384dd5008624dbb8b7630d64ea344241c76436
shafrianadhi <shafrianadhi@gmail.com>
2018-03-15 12:52:28 +0700

    remove

--- expressmongodb/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-var path = require('path')
-exports.article = require(path.join(__dirname, 'article'));
-exports.upload = require(path.join(__dirname, 'upload'));
-
-// GET home page.
-exports.index = (req, res, next) => {
-      res.send('<h1>This is Homepage</h1>');
-}
--- expressmongodb/seed.sh
+++
@@ -1,1 +0,0 @@
-mongoimport --host=127.0.0.1 --db express_app --collection articles --file ./db/articles.json --jsonArray
