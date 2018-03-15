b51999c57676915ba8acbc71fd13693846f1e223
shafrianadhi <shafrianadhi@gmail.com>
2018-03-15 12:53:13 +0700

    remove

--- webcrawler/routes/api/index.js
+++
@@ -1,7 +0,0 @@
-var path = require('path')
-exports.article = require(path.join(__dirname, 'article'));
-
-// GET home page.
-exports.index = (req, res, next) => {
-      res.send('<h1>This is Homepage</h1>');
-}
